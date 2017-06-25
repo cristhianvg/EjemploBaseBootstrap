d2c6acec324bb964428fae717df4a8b20ca29618
cristhianvg <cristhianvg1991@gmail.com>
2017-06-25 11:49:23 -0500

    INICIO DE UN PROYECTO BASE 1. CONTIENE: - Libreria de bootstrap con sus respectivas carpetas (CSS, FONTS, JS) - Documento "index.html" con sus respectivos librerias cargadas - Hoja de estilo "estiloIndex.css" enlazada al "index.html" lista para agregar los estilos y ser utilizada
    2. FUNCIONALIDAD: 
    - El "index.html" funciona al 100% en la Web con un simple texto
    - Carga los estilos de la Hoja de estilos ("estiloIndex.css")
    
    NOTA: Al tener Bootstrap se convierte en una aplicacion response (PC, 
    TABLES, CELULAR) la cual funciona al 100% en la Web

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/estiloIndex.css">
+  </head>
+  <body>
+    <div class="container-fluid">
+      <div class="row"> 
+        <div class="col-lg-2 col-lg-offset-3 col-xs-12">
+          <h1 class="titulo">Lo has hecho bien !!</h1>
+        </div>
+      </div>
+    </div>
+
+    <!-- librerias jquery -->
+    <script src="js/jquery-1.12.4.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
